6041540851da263af8c2a0ba58deb0fb172a17e6
GIRISH GOWDA B A <151643817+apgis@users.noreply.github.com>
2025-03-06 00:53:42 +0530

    Update heart_desease_web_app.py

--- heart_desease_web_app.py
+++ heart_desease_web_app.py
@@ -17,7 +17,7 @@ from streamlit_option_menu import option_menu
 #feature_names = df.drop(columns=["target"]).columns  # Replace "target" with actual target column name
 
 #print("Selected Feature Names:", feature_names.tolist())
-loaded_model=pickle.load(open('C:/Users/User/deployment_model/heart_disease_model.sav','rb'))
+loaded_model=pickle.load(open('deployment_model/heart_disease_model.sav','rb'))
 with st.sidebar:
     selected = option_menu('MULTIPLE DESEASE PREDICTION',
                          ['heart desease prediction','diabetes prediction'],icons=['heart','activity'],
@@ -26,10 +26,10 @@ with st.sidebar:
 def heart(input_list):
     
 
-# change the input data to a numpy array
+    # change the input data to a numpy array
     input_data_as_numpy_array= np.array(input_list, dtype=np.float64)
 
-# reshape the numpy array as we are predicting for only on instance
+    # reshape the numpy array as we are predicting for only on instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
     prediction = loaded_model.predict(input_data_reshaped)
@@ -41,26 +41,26 @@ def heart(input_list):
     else:
        return 'The Person has Heart Disease'
 if selected=='heart desease prediction':   
-  def main():
-    st.title('heart desease pediction')
-    age=st.text_input('age')
-    sex=st.text_input('sex')
-    cp=st.text_input('cp')
-    trestbps=st.text_input('trestbps')
-    chol=st.text_input('chol')
-    fbs=st.text_input('fbs')
-    restecg=st.text_input('restecg')
-    talach=st.text_input('talach')
-    exang=st.text_input('exang')
-    oldpeak=st.text_input('oldpeak')
-    slope=st.text_input('slope')
-    ca=st.text_input('ca')
-    thal=st.text_input('thal')
-    diagnosis=''
+   def main():
+      st.title('heart desease pediction')
+      age=st.text_input('age')
+      sex=st.text_input('sex')
+      cp=st.text_input('cp')
+      trestbps=st.text_input('trestbps')
+      chol=st.text_input('chol')
+      fbs=st.text_input('fbs')
+      restecg=st.text_input('restecg')
+      talach=st.text_input('talach')
+      exang=st.text_input('exang')
+      oldpeak=st.text_input('oldpeak')
+      slope=st.text_input('slope')
+      ca=st.text_input('ca')
+      thal=st.text_input('thal')
+      diagnosis=''
     
-    if st.button('heart disease prediction result'):
-        diagnosis=heart([age,sex,cp,trestbps,chol,fbs,restecg,talach,exang,oldpeak,slope,ca,thal])
-    st.success(diagnosis)   
+      if st.button('heart disease prediction result'):
+         diagnosis=heart([age,sex,cp,trestbps,chol,fbs,restecg,talach,exang,oldpeak,slope,ca,thal])
+      st.success(diagnosis)   
     
 if __name__=='__main__':
         main()
